d52a01f4dab2408d6658a2e2bb01c507940f3665
ECPAY\shawn.chang <shawn.chang@greenworld.com.tw>
2016-11-25 18:02:42 +0800

    20161125

---
+++ app/Mem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mem extends Model
+{
+    /**
+     * Table name
+     * @var string
+     */
+    protected $table = 'mem';
+}
